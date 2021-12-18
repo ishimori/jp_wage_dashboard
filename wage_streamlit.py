caf566026b360533eb5e027e90123d73e01a3faa
ishimori <ishimori_yuzuru@probsc.jp>
2021-12-18 20:13:00 +0900

    fix path

--- wage_streamlit.py
+++ wage_streamlit.py
@@ -7,14 +7,11 @@ st.title('日本の賃金データのダッシュボード')
 df_jp_ind = pd.read_csv('./csv_data/df_jp_ind.csv', encoding='shift_jis')
 df_jp_category = pd.read_csv('./csv_data/df_jp_category.csv', encoding='shift_jis')
 df_pref_ind = pd.read_csv('./csv_data/df_pref_ind.csv', encoding='shift_jis')
-# df_jp_ind = pd.read_csv('.\csv_data\df_jp_ind.csv', encoding='shift_jis')
-# df_jp_category = pd.read_csv('.\csv_data\df_jp_category.csv', encoding='shift_jis')
-# df_pref_ind = pd.read_csv('.\csv_data\df_pref_ind.csv', encoding='shift_jis')
 
 st.header('■2019年一人あたり平均賃金のヒートマップ')
-jp_lat_lon = pd.read_csv('.\csv_data\pref_lat_lon.csv')
+jp_lat_lon = pd.read_csv('./csv_data/pref_lat_lon.csv')
 jp_lat_lon = jp_lat_lon.rename(columns={'pref_name':'都道府県名'})
-# jp_lat_lon
+
 
 df_pref_map = df_pref_ind[(df_pref_ind['年齢'] == '年齢計') & (df_pref_ind['集計年'] == 2019)]
 df_pref_map = pd.merge(df_pref_map, jp_lat_lon, on='都道府県名')
